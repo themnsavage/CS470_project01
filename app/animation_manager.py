61b09238d9e0a0b184d56b1d8039a0c5deee5766
katieperkins1 <110872407+katieperkins1@users.noreply.github.com>
2023-01-23 10:30:55 -0600

    Update animation_manager.py

--- app/animation_manager.py
+++ app/animation_manager.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt, animation
 from app.sorting_algorithms import Sorting_Algorithms
 
+
 class Animation_Manager:
     def __init__(self, k_digits = 1, data = []):
         self._sorting_algorithms = Sorting_Algorithms()
@@ -9,11 +10,14 @@ class Animation_Manager:
         self._data_size = len(data) + 1
 
     def visualize_bubble_sort(self):
+        '''
+            Description: Visualization of the quick sort algorithm
+        '''
         data = self._data.copy()
         generator = self._sorting_algorithms.bubble_sort(data, self._k_digits)
 
         fig, ax = plt.subplots()
-        ax.set_title(f"Bubble Sort O(n\N{SUPERSCRIPT TWO}) with k = {self._k_digits}")
+        ax.set_title(f"Bubble Sort O(n\N{SUPERSCRIPT TWO}) with k = {self._k_digits}")  #set title for the graph
         bar_sub = ax.bar(range(len(self._data)), self._data, align="edge")
 
         ax.set_xlim(0, self._data_size)
@@ -21,6 +25,12 @@ class Animation_Manager:
         iteration = [0]
 
         def update(data, rects, iteration):
+            '''
+               Description: Checks if sorting is completed and is correct
+               data: Input list to be sorted
+               rects: Object of rectangle class from matplotlib
+               iteration: Number of operations in sort
+            '''
             if data['is_sorting_done'] is False:
                 for rect, val in zip(rects, data['list']):
                     rect.set_height(val)
@@ -32,7 +42,7 @@ class Animation_Manager:
                     if index >= (self._data_size - 1) - self._k_digits:
                         rect.set(color='green') # change the kth top digits bar color to green
                     index += 1
-
+                    
         anim = animation.FuncAnimation(
         fig,
         func=update,
@@ -48,11 +58,14 @@ class Animation_Manager:
         plt.close()
 
     def visualize_quick_sort(self):
+        '''
+            Description: Visualization of the quick sort algorithm
+        '''
         data = self._data.copy()
         generator = self._sorting_algorithms.quicksort(data, 0, len(data)-1, self._k_digits)
 
         fig, ax = plt.subplots()
-        ax.set_title(f"Quick Sort O(n) with k = {self._k_digits}")
+        ax.set_title(f"Quick Sort O(n) with k = {self._k_digits}")  #set title for the graph
         bar_sub = ax.bar(range(len(self._data)), self._data, align="edge")
 
         ax.set_xlim(0, self._data_size)
@@ -60,6 +73,12 @@ class Animation_Manager:
         iteration = [0]
 
         def update(data, rects, iteration):
+            '''
+               Description: Checks if sorting is completed and is correct
+               data: Input list to be sorted
+               rects: Object of rectangle class from matplotlib
+               iteration: Number of operations in sort
+            '''
             if data['is_sorting_done'] is False:
                 index = 0
                 for rect, val in zip(rects, data['list']):
