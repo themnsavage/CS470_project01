55d616135367fe5c919761ddd084749ec5b5c974
BrysonDucky <brysonaduckworth@gmail.com>
2023-01-23 16:24:54 +0000

    doc(main.py): add comments to main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from random import shuffle
+from random import shuffle, randint
 from app.animation_manager import Animation_Manager
 
 def main():
-    data = list(range(1, 31))
+    #creating a list of data
+    data = list(range(1, 31)) 
+    #randomizing the list
     shuffle(data)
-
-    animation_manager = Animation_Manager(data=data, k_digits=5)
+    #choosing a random k value that is within the data set
+    random_k_digits = randint(1, len(data)) 
+    #sending data to Animation_Manager for generating the plots
+    animation_manager = Animation_Manager(data=data, k_digits=random_k_digits)
+    #creating the plots for each sorting algorithm
     animation_manager.visualize_bubble_sort()
     animation_manager.visualize_quick_sort()
 
